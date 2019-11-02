098c0280029164a8148c2bd9d42aad98defc9249
cristian-nitu <55763479+cristian-nitu@users.noreply.github.com>
2019-11-02 05:55:24 -0400

    dataframe

--- pandas-excercise.py
+++ pandas-excercise.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 
-sr = pd.read_csv('c:\python37\doc\insurance.csv')
+sr = pd.read_csv('c:\python37\doc\insurance.csv').head(5)
 
 sr.to_string()
-print(sr)
+#print(sr)
+print(sr.age, sr.children, sr.charges)
+
